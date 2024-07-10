7e7dd713589bde520326f20cd747527fbb82c53b
RaederDev <github@raeder.dev>
2024-07-10 23:57:11 +0200

    start working on display

--- src/display.rs
+++ src/display.rs
@@ -1,66 +1,87 @@
-// #define EPD_SPI_SCK 47
-// #define EPD_SPI_MISO 46
-// #define EPD_SPI_MOSI 48
-// #define EPD_SPI_SS 33
-
+use anyhow::Result;
+use embedded_graphics::geometry::Point;
+use embedded_graphics::prelude::Primitive;
+use embedded_graphics::primitives::{Circle, Line, PrimitiveStyle, PrimitiveStyleBuilder};
+use embedded_graphics::Drawable;
+use epd_waveshare::color::Color;
+use epd_waveshare::epd1in54::{Display1in54, Epd1in54};
+use epd_waveshare::prelude::WaveshareDisplay;
 use esp_idf_svc::hal::delay::Delay;
-use esp_idf_svc::hal::gpio::{AnyInputPin, AnyOutputPin};
+use esp_idf_svc::hal::gpio::{AnyInputPin, AnyOutputPin, Input, Output, PinDriver};
 use esp_idf_svc::hal::spi::config::{Config, DriverConfig};
-use esp_idf_svc::hal::spi::{SpiDeviceDriver, SPI2};
+use esp_idf_svc::hal::spi::{SpiDeviceDriver, SpiDriver, SPI2};
 use esp_idf_svc::hal::units::FromValueType;
 
-pub struct WatchyDisplay;
-
-impl WatchyDisplay {
-    pub fn init(
-        spi2: SPI2,
-        pin_sck: AnyOutputPin,
-        pin_miso: AnyInputPin,
-        pin_mosi: AnyOutputPin,
-        pin_ss: AnyOutputPin,
-    ) {
-        let mut spi = SpiDeviceDriver::new_single(
-            spi2,
-            pin_sck,
-            pin_mosi,
-            Some(pin_miso),
-            Some(pin_ss),
-            &DriverConfig::new(),
-            &Config::new().baudrate(20.MHz().into()),
-        );
-        let mut delay = Delay::new_default();
-        //
-        // let mut spi = Spi::new(spi2, 20.MHz(), SpiMode::Mode0, clocks).with_pins(
-        //     Some(pin_spi_sck),
-        //     Some(pin_spi_mosi),
-        //     Some(pin_spi_miso),
-        //     Some(pin_spi_ss),
-        // );
-        // let mut delay = Delay;
-        // let mut epd = Epd1in54::new(&mut spi, OutputPin, busy_in, dc, rst, &mut delay);
-    }
+pub struct WatchyDisplay<'a> {
+    epd: Epd1in54<
+        SpiDeviceDriver<'a, SpiDriver<'a>>,
+        PinDriver<'a, AnyInputPin, Input>,
+        PinDriver<'a, AnyOutputPin, Output>,
+        PinDriver<'a, AnyOutputPin, Output>,
+        Delay,
+    >,
+    spi: SpiDeviceDriver<'a, SpiDriver<'a>>,
+    delay: Delay,
 }
 
-fn setup_display() {
-    // Setup EPD
-    // let mut epd = Epd1in54::new(&mut spi, cs_pin, busy_in, dc, rst, &mut delay)?;
-    //
-    // // Use display graphics from embedded-graphics
-    // let mut display = Display1in54::default();
-    //
-    // // Use embedded graphics for drawing a line
-    // let style = PrimitiveStyleBuilder::new()
-    //     .stroke_color(Black)
-    //     .stroke_width(1)
-    //     .build();
-    // let _ = Line::new(Point::new(0, 120), Point::new(0, 295))
-    //     .into_styled(style)
-    //     .draw(&mut display);
-    //
-    // // Display updated frame
-    // epd.update_frame(&mut spi, &display.buffer(), &mut delay)?;
-    // epd.display_frame(&mut spi, &mut delay)?;
-    //
-    // // Set the EPD to sleep
-    // epd.sleep(&mut spi, &mut delay)?;
+impl<'a> WatchyDisplay<'a> {
+    pub fn new(
+        spi2: SPI2,
+        pin_spi_sck: AnyOutputPin,
+        pin_spi_miso: AnyInputPin,
+        pin_spi_mosi: AnyOutputPin,
+        pin_spi_edp_ss: AnyOutputPin,
+        pin_edp_dc: AnyOutputPin,
+        pin_edp_reset: AnyOutputPin,
+        pin_edp_busy: AnyInputPin,
+    ) -> Result<WatchyDisplay<'a>> {
+        let mut spi = SpiDeviceDriver::new_single(
+            spi2,
+            pin_spi_sck,
+            pin_spi_mosi,
+            Some(pin_spi_miso),
+            Some(pin_spi_edp_ss),
+            &DriverConfig::new(),
+            &Config::new().baudrate(20.MHz().into()),
+        )
+        .unwrap();
+        let mut delay = Delay::new_default();
+        let epd = Epd1in54::new(
+            &mut spi,
+            PinDriver::input(pin_edp_busy)?,
+            PinDriver::output(pin_edp_dc)?,
+            PinDriver::output(pin_edp_reset)?,
+            &mut delay,
+            None,
+        )
+        .unwrap();
+
+        Ok(WatchyDisplay { epd, spi, delay })
+    }
+
+    pub fn draw_test(&mut self) -> Result<()> {
+        // Use display graphics from embedded-graphics
+        let mut display = Display1in54::default();
+
+        // Use embedded graphics for drawing a line
+        let style = PrimitiveStyleBuilder::new()
+            .stroke_color(Color::Black)
+            .stroke_width(1)
+            .build();
+        let _ = Line::new(Point::new(0, 120), Point::new(0, 295))
+            .into_styled(style)
+            .draw(&mut display);
+        let _ = Circle::with_center(Point::new(50, 50), 50)
+            .into_styled(PrimitiveStyle::with_fill(Color::White))
+            .draw(&mut display);
+
+        // Display updated frame
+        self.epd
+            .update_frame(&mut self.spi, &display.buffer(), &mut self.delay)?;
+        self.epd.display_frame(&mut self.spi, &mut self.delay)?;
+
+        // Set the EPD to sleep
+        self.epd.sleep(&mut self.spi, &mut self.delay)?;
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,22 @@ fn main() -> anyhow::Result<()> {
     let pin_spi_sck = peripherals.pins.gpio47;
     let pin_spi_miso = peripherals.pins.gpio46;
     let pin_spi_mosi = peripherals.pins.gpio48;
-    let pin_spi_ss = peripherals.pins.gpio33;
-    WatchyDisplay::init(
+    let pin_spi_edp_ss = peripherals.pins.gpio33;
+    let pin_edp_dc = peripherals.pins.gpio34;
+    let pin_edp_reset = peripherals.pins.gpio35;
+    let pin_edp_busy = peripherals.pins.gpio36;
+    let mut display = WatchyDisplay::new(
         spi2,
         pin_spi_sck.into(),
         pin_spi_miso.into(),
         pin_spi_mosi.into(),
-        pin_spi_ss.into(),
-    );
+        pin_spi_edp_ss.into(),
+        pin_edp_dc.into(),
+        pin_edp_reset.into(),
+        pin_edp_busy.into(),
+    )
+    .expect("Failed to initialize display");
+    display.draw_test().expect("Failed to draw test");
 
     log::info!("Hello, world!");
     Ok(())
